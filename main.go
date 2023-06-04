b31d094da3d560f3e089d839f80ee0f1151be5ea
miyazi777 <m71203@gmail.com>
2023-06-04 16:30:07 +0900

    P138 起動ポートを動的に変更できるようにする

--- main.go
+++ main.go
@@ -4,21 +4,35 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"net"
 	"net/http"
+	"os"
 
 	"golang.org/x/sync/errgroup"
 )
 
 func main() {
-	if err := run(context.Background()); err != nil {
+	if len(os.Args) != 2 {
+		log.Printf("need port number\n")
+		os.Exit(1)
+	}
+
+	p := os.Args[1]
+	l, err := net.Listen("tcp", ":"+p)
+	if err != nil {
+		log.Fatalf("failed to listen port %s: %v", p, err)
+	}
+
+	if err := run(context.Background(), l); err != nil {
 		log.Printf("failed to terminate server: %v", err)
+		os.Exit(1)
 	}
 }
 
-func run(ctx context.Context) error {
+func run(ctx context.Context, l net.Listener) error {
 	// 起動するサーバとハンドラーを用意する
 	s := &http.Server{
-		Addr: ":18080",
+		// Addr: ":18080",	引数で受け取ったListenerを使うので、Addrは指定しない
 		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
 		}),
@@ -28,7 +42,7 @@ func run(ctx context.Context) error {
 	// 別ゴルーチンでHTTPサーバを起動する
 	eg.Go(func() error {
 		// http.ErrServerClosedはhttp.Server.Shutdown()が正常に終了したことを示すので異常ではない
-		if err := s.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		if err := s.Serve(l); err != nil && err != http.ErrServerClosed {
 			log.Printf("failed to close: %+v", err)
 			return err
 		}

31f0ca06dcc587029eafc4b3abf7314a5df58e69
miyazi777 <m71203@gmail.com>
2023-06-08 23:06:37 +0900

    P164 URLのルーティング部分をmux.goとして分離

--- main.go
+++ main.go
@@ -5,14 +5,9 @@ import (
 	"fmt"
 	"log"
 	"net"
-	"net/http"
 	"os"
-	"os/signal"
-	"syscall"
-	"time"
 
 	"github.com/miyazi777/go-todo-app2/config"
-	"golang.org/x/sync/errgroup"
 )
 
 func main() {
@@ -23,10 +18,6 @@ func main() {
 }
 
 func run(ctx context.Context) error {
-	// SIGTERMを受け取ったら、現在の処理中の処理の終了を待ってから終了する
-	ctx, stop := signal.NotifyContext(ctx, os.Interrupt, syscall.SIGTERM)
-	defer stop()
-
 	cfg, err := config.New()
 	if err != nil {
 		return err
@@ -35,37 +26,56 @@ func run(ctx context.Context) error {
 	if err != nil {
 		log.Fatalf("failed to listen port %d: %v", cfg.Port, err)
 	}
+
 	url := fmt.Sprintf("http://%s", l.Addr().String())
 	log.Printf("start with: %v", url)
+	mux := NewMux()
+	s := NewServer(l, mux)
+	return s.Run(ctx)
 
-	// 起動するサーバとハンドラーを用意する
-	s := &http.Server{
-		// Addr: ":18080",	引数で受け取ったListenerを使うので、Addrは指定しない
-		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			time.Sleep(5 * time.Second)
-			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
-		}),
-	}
-	eg, ctx := errgroup.WithContext(ctx)
+	// // SIGTERMを受け取ったら、現在の処理中の処理の終了を待ってから終了する
+	// ctx, stop := signal.NotifyContext(ctx, os.Interrupt, syscall.SIGTERM)
+	// defer stop()
 
-	// 別ゴルーチンでHTTPサーバを起動する
-	eg.Go(func() error {
-		// http.ErrServerClosedはhttp.Server.Shutdown()が正常に終了したことを示すので異常ではない
-		if err := s.Serve(l); err != nil && err != http.ErrServerClosed {
-			log.Printf("failed to close: %+v", err)
-			return err
-		}
-		return nil
-	})
+	// cfg, err := config.New()
+	// if err != nil {
+	// 	return err
+	// }
+	// l, err := net.Listen("tcp", fmt.Sprintf(":%d", cfg.Port))
+	// if err != nil {
+	// 	log.Fatalf("failed to listen port %d: %v", cfg.Port, err)
+	// }
+	// url := fmt.Sprintf("http://%s", l.Addr().String())
+	// log.Printf("start with: %v", url)
 
-	// チャネルからの通知（終了通知）を待機する
-	<-ctx.Done()
+	// // 起動するサーバとハンドラーを用意する
+	// s := &http.Server{
+	// 	// Addr: ":18080",	引数で受け取ったListenerを使うので、Addrは指定しない
+	// 	Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	// 		time.Sleep(5 * time.Second)
+	// 		fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+	// 	}),
+	// }
+	// eg, ctx := errgroup.WithContext(ctx)
 
-	// http.Serverのshutdownを呼び出すのでグレースフルシャットダウンを開始する
-	if err := s.Shutdown(context.Background()); err != nil {
-		log.Printf("failed to shutdown: %+v", err)
-	}
+	// // 別ゴルーチンでHTTPサーバを起動する
+	// eg.Go(func() error {
+	// 	// http.ErrServerClosedはhttp.Server.Shutdown()が正常に終了したことを示すので異常ではない
+	// 	if err := s.Serve(l); err != nil && err != http.ErrServerClosed {
+	// 		log.Printf("failed to close: %+v", err)
+	// 		return err
+	// 	}
+	// 	return nil
+	// })
 
-	// Goメソッドで起動した別ゴルーチンの終了を待つ
-	return eg.Wait()
+	// // チャネルからの通知（終了通知）を待機する
+	// <-ctx.Done()
+
+	// // http.Serverのshutdownを呼び出すのでグレースフルシャットダウンを開始する
+	// if err := s.Shutdown(context.Background()); err != nil {
+	// 	log.Printf("failed to shutdown: %+v", err)
+	// }
+
+	// // Goメソッドで起動した別ゴルーチンの終了を待つ
+	// return eg.Wait()
 }
